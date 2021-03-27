2f0186ed1740b9c30c63475bb7eb92b13ae24116
dahunta07 <hunter.mayne@gmail.com>
2021-03-27 20:59:41 +1100

    MVP

--- ZLheim_Modloader/Form1.Designer.cs
+++ ZLheim_Modloader/Form1.Designer.cs
@@ -28,33 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
             this.materialLabel3 = new MaterialSkin.Controls.MaterialLabel();
             this.materialLabel4 = new MaterialSkin.Controls.MaterialLabel();
             this.materialLabel5 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel6 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel7 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialRaisedButton4 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialLabel8 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel9 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialProgressBar1 = new MaterialSkin.Controls.MaterialProgressBar();
+            this.materialRaisedButton1 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::ZLheim_Modloader.Properties.Resources.Logo_ZLalheim_w300;
-            this.pictureBox1.Location = new System.Drawing.Point(252, 44);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(298, 98);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // materialLabel2
             // 
             this.materialLabel2.AutoSize = true;
             this.materialLabel2.Depth = 0;
-            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel2.Location = new System.Drawing.Point(50, 230);
+            this.materialLabel2.Location = new System.Drawing.Point(61, 207);
             this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel2.Name = "materialLabel2";
-            this.materialLabel2.Size = new System.Drawing.Size(176, 19);
+            this.materialLabel2.Size = new System.Drawing.Size(173, 18);
             this.materialLabel2.TabIndex = 1;
             this.materialLabel2.Text = "Valhiem Game Directory:";
             // 
@@ -62,12 +61,12 @@
             // 
             this.materialLabel3.AutoSize = true;
             this.materialLabel3.Depth = 0;
-            this.materialLabel3.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.materialLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel3.Location = new System.Drawing.Point(33, 265);
+            this.materialLabel3.Location = new System.Drawing.Point(107, 330);
             this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel3.Name = "materialLabel3";
-            this.materialLabel3.Size = new System.Drawing.Size(193, 19);
+            this.materialLabel3.Size = new System.Drawing.Size(184, 18);
             this.materialLabel3.TabIndex = 2;
             this.materialLabel3.Text = "Valheim Plus Install Status:";
             this.materialLabel3.Click += new System.EventHandler(this.materialLabel3_Click);
@@ -76,12 +75,12 @@
             // 
             this.materialLabel4.AutoSize = true;
             this.materialLabel4.Depth = 0;
-            this.materialLabel4.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.materialLabel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel4.Location = new System.Drawing.Point(248, 230);
+            this.materialLabel4.Location = new System.Drawing.Point(256, 207);
             this.materialLabel4.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel4.Name = "materialLabel4";
-            this.materialLabel4.Size = new System.Drawing.Size(108, 19);
+            this.materialLabel4.Size = new System.Drawing.Size(103, 18);
             this.materialLabel4.TabIndex = 3;
             this.materialLabel4.Text = "materialLabel4";
             // 
@@ -89,18 +88,144 @@
             // 
             this.materialLabel5.AutoSize = true;
             this.materialLabel5.Depth = 0;
-            this.materialLabel5.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.materialLabel5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel5.Location = new System.Drawing.Point(248, 265);
+            this.materialLabel5.Location = new System.Drawing.Point(322, 330);
             this.materialLabel5.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel5.Name = "materialLabel5";
-            this.materialLabel5.Size = new System.Drawing.Size(108, 19);
+            this.materialLabel5.Size = new System.Drawing.Size(103, 18);
             this.materialLabel5.TabIndex = 4;
             this.materialLabel5.Text = "materialLabel5";
             // 
+            // materialLabel6
+            // 
+            this.materialLabel6.AutoSize = true;
+            this.materialLabel6.Depth = 0;
+            this.materialLabel6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.materialLabel6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel6.Location = new System.Drawing.Point(82, 362);
+            this.materialLabel6.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel6.Name = "materialLabel6";
+            this.materialLabel6.Size = new System.Drawing.Size(208, 18);
+            this.materialLabel6.TabIndex = 5;
+            this.materialLabel6.Text = "Installed Valheim Plus Version:";
+            this.materialLabel6.Click += new System.EventHandler(this.materialLabel6_Click);
+            // 
+            // materialLabel7
+            // 
+            this.materialLabel7.AutoSize = true;
+            this.materialLabel7.Depth = 0;
+            this.materialLabel7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.materialLabel7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel7.Location = new System.Drawing.Point(322, 362);
+            this.materialLabel7.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel7.Name = "materialLabel7";
+            this.materialLabel7.Size = new System.Drawing.Size(103, 18);
+            this.materialLabel7.TabIndex = 6;
+            this.materialLabel7.Text = "materialLabel7";
+            // 
+            // materialRaisedButton4
+            // 
+            this.materialRaisedButton4.AutoSize = true;
+            this.materialRaisedButton4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialRaisedButton4.Depth = 0;
+            this.materialRaisedButton4.Icon = null;
+            this.materialRaisedButton4.Location = new System.Drawing.Point(180, 475);
+            this.materialRaisedButton4.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton4.Name = "materialRaisedButton4";
+            this.materialRaisedButton4.Primary = true;
+            this.materialRaisedButton4.Size = new System.Drawing.Size(94, 36);
+            this.materialRaisedButton4.TabIndex = 8;
+            this.materialRaisedButton4.Text = "Uninstall";
+            this.materialRaisedButton4.UseVisualStyleBackColor = true;
+            this.materialRaisedButton4.Click += new System.EventHandler(this.materialRaisedButton4_Click);
+            // 
+            // materialLabel8
+            // 
+            this.materialLabel8.AutoSize = true;
+            this.materialLabel8.Depth = 0;
+            this.materialLabel8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.materialLabel8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel8.Location = new System.Drawing.Point(107, 274);
+            this.materialLabel8.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel8.Name = "materialLabel8";
+            this.materialLabel8.Size = new System.Drawing.Size(93, 18);
+            this.materialLabel8.TabIndex = 9;
+            this.materialLabel8.Text = "Valheim Plus";
+            this.materialLabel8.Click += new System.EventHandler(this.materialLabel8_Click);
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(97, 393);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(195, 18);
+            this.materialLabel1.TabIndex = 10;
+            this.materialLabel1.Text = "Latest Valheim Plus Version:";
+            this.materialLabel1.Click += new System.EventHandler(this.materialLabel1_Click_1);
+            // 
+            // materialLabel9
+            // 
+            this.materialLabel9.AutoSize = true;
+            this.materialLabel9.Depth = 0;
+            this.materialLabel9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.materialLabel9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel9.Location = new System.Drawing.Point(322, 393);
+            this.materialLabel9.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel9.Name = "materialLabel9";
+            this.materialLabel9.Size = new System.Drawing.Size(103, 18);
+            this.materialLabel9.TabIndex = 11;
+            this.materialLabel9.Text = "materialLabel9";
+            // 
+            // materialProgressBar1
+            // 
+            this.materialProgressBar1.Depth = 0;
+            this.materialProgressBar1.Location = new System.Drawing.Point(121, 433);
+            this.materialProgressBar1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialProgressBar1.Name = "materialProgressBar1";
+            this.materialProgressBar1.Size = new System.Drawing.Size(304, 5);
+            this.materialProgressBar1.TabIndex = 12;
+            // 
+            // materialRaisedButton1
+            // 
+            this.materialRaisedButton1.AutoSize = true;
+            this.materialRaisedButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialRaisedButton1.Depth = 0;
+            this.materialRaisedButton1.Icon = null;
+            this.materialRaisedButton1.Location = new System.Drawing.Point(99, 475);
+            this.materialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton1.Name = "materialRaisedButton1";
+            this.materialRaisedButton1.Primary = true;
+            this.materialRaisedButton1.Size = new System.Drawing.Size(75, 36);
+            this.materialRaisedButton1.TabIndex = 13;
+            this.materialRaisedButton1.Text = "Install";
+            this.materialRaisedButton1.UseVisualStyleBackColor = true;
+            this.materialRaisedButton1.Click += new System.EventHandler(this.materialRaisedButton1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ZLheim_Modloader.Properties.Resources.Logo_ZLalheim_w300;
+            this.pictureBox1.Location = new System.Drawing.Point(232, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(298, 98);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(745, 813);
+            this.ClientSize = new System.Drawing.Size(747, 555);
+            this.Controls.Add(this.materialRaisedButton1);
+            this.Controls.Add(this.materialProgressBar1);
+            this.Controls.Add(this.materialLabel9);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.materialLabel8);
+            this.Controls.Add(this.materialRaisedButton4);
+            this.Controls.Add(this.materialLabel7);
+            this.Controls.Add(this.materialLabel6);
             this.Controls.Add(this.materialLabel5);
             this.Controls.Add(this.materialLabel4);
             this.Controls.Add(this.materialLabel3);
@@ -116,17 +241,19 @@
 
         #endregion
 
-        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton1;
-        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton2;
-        private System.Windows.Forms.PictureBox pictureBox_TitleGraphic;
-        private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField_ValhiemDirectoryStatus;
-        private MaterialSkin.Controls.MaterialLabel materialLabel1;
-        private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextFieldValhiemPlusInstallStatus;
         private System.Windows.Forms.PictureBox pictureBox1;
         private MaterialSkin.Controls.MaterialLabel materialLabel2;
         private MaterialSkin.Controls.MaterialLabel materialLabel3;
         private MaterialSkin.Controls.MaterialLabel materialLabel4;
         private MaterialSkin.Controls.MaterialLabel materialLabel5;
+        private MaterialSkin.Controls.MaterialLabel materialLabel6;
+        private MaterialSkin.Controls.MaterialLabel materialLabel7;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton4;
+        private MaterialSkin.Controls.MaterialLabel materialLabel8;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel9;
+        private MaterialSkin.Controls.MaterialProgressBar materialProgressBar1;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton1;
     }
 }
 
--- ZLheim_Modloader/Form1.cs
+++ ZLheim_Modloader/Form1.cs
@@ -10,19 +10,43 @@ using System.Windows.Forms;
 
 
 using System.IO;
+using System.IO.Compression;
 using System.Collections;
 using MaterialSkin;
 using MaterialSkin.Controls;
 using Microsoft.Win32;
+using System.Diagnostics;
+using Octokit;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace ZLheim_Modloader
 {
-
+    
     public partial class Form1 : MaterialForm
     {
 
-        string DefaultSteamInstallDir = @"C:\Program Files (x86)\Steam\steamapps\common\Valheim";
-        string ValheimGameFolder;
+        private const string DefaultSteamInstallDir = @"C:\Program Files (x86)\Steam\steamapps\common\Valheim";
+        private static FileVersionInfo ValheimPlusFileDetail;
+
+        private static string ValheimGameFolder;
+        private static string ValheimPlusDll;
+        private static string ValheimPlusInstallStatus;
+        private static string ValheimPlusVersion; 
+        private static string LatestValheimPlusVersion;
+        //private List<string[]> releases = new List<string[]>();
+
+        private dynamic JsonResponse;
+        private bool downloadComplete = false;
+
+
+        
+        private static string TempPath = Path.GetTempPath();
+        private static string ZipFullPath = $"{TempPath}WindowsClient.zip";
+        private static string ZipExtractionPath = $"{TempPath}WindowsClient";
+
 
         public Form1()
         {
@@ -98,35 +122,233 @@ namespace ZLheim_Modloader
             return string.Empty;
         }
 
+        private void UpdateUI()
+        {
+            materialLabel4.Text = ValheimGameFolder; //Game install Status
+            materialLabel4.Refresh();
+            materialLabel5.Text = ValheimPlusInstallStatus; //Valhiem Plus Install Status
+            materialLabel5.Refresh();
+            materialLabel7.Text = ValheimPlusVersion; //Valheim Plus Version Installed
+            materialLabel7.Refresh();
+            materialLabel9.Text = LatestValheimPlusVersion; //Latest Valheim Plus Version from github
+            materialLabel9.Refresh();
 
+        }
 
-        private string LocateValhiemDirectory()
+        private void LocateValheimDirectory()
         {
 
             if (Directory.Exists(DefaultSteamInstallDir))
             {
-                return DefaultSteamInstallDir;
+                ValheimPlusInstallStatus = DefaultSteamInstallDir;
+                ValheimGameFolder = DefaultSteamInstallDir;
+                UpdateUI();
             }
             else
             {
-                return "Valhiem install directory not found!";
+                ValheimPlusInstallStatus = "Valheim install directory not found!";
+                ValheimGameFolder = null;
+                UpdateUI();
             }
 
         }
 
-        private static bool GetValhiemPlusInstallStatus(string RootGameDirectory)
+        private void GetValheimPlusInstallStatus()
         {
-            string ValheimPlusDll = $"{RootGameDirectory}\\BepInEx\\plugins\\ValheimPlus.dll";
+            ValheimPlusDll = $"{ValheimGameFolder}\\BepInEx\\plugins\\ValheimPlus.dll";
 
             if (File.Exists(ValheimPlusDll))
             {
-                return true;
+                ValheimPlusInstallStatus = "Valheim Plus is Installed";
+                UpdateUI();
             }
             else
             {
-                return false;
+                ValheimPlusInstallStatus = "Valheim Plus is not Installed";
+                ValheimPlusVersion = "-";
+                UpdateUI();
+            }
+            
+        }
+
+        private void GetValheimPlusVersion()
+        {
+
+            GetValheimPlusInstallStatus();
+            if (File.Exists(ValheimPlusDll))
+            {
+                ValheimPlusFileDetail = FileVersionInfo.GetVersionInfo(ValheimPlusDll);
+                ValheimPlusVersion = ValheimPlusFileDetail.FileVersion;
+                UpdateUI();
+            }
+            else
+            {
+                ValheimPlusVersion = "-";
+                UpdateUI();
             }
 
+        }
+
+        private void GetLatestValheimPlusVersion()
+        {
+
+
+
+            var client = new RestClient("https://api.github.com");
+            client.Authenticator = new HttpBasicAuthenticator("username", "password");
+
+            var request = new RestRequest("/repos/valheimPlus/ValheimPlus/releases/latest", DataFormat.Json);
+
+            var response = client.Get(request);
+            JsonResponse = JsonConvert.DeserializeObject(response.Content);
+            LatestValheimPlusVersion = JsonResponse.tag_name;
+            UpdateUI();
+
+
+
+
+        }
+
+        private void ExitandCleanup()
+        {
+            if (File.Exists(ZipFullPath))
+            {
+                File.Delete(ZipFullPath);
+            }
+
+            if (Directory.Exists(ZipExtractionPath))
+            {
+                Directory.Delete(ZipExtractionPath,true);
+            }
+
+        }
+
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            materialProgressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            ValheimPlusInstallStatus = "Download completed!";
+            UpdateUI();
+            downloadComplete = true;
+        }
+        private void InstallValheimPlus()
+        {
+            GetLatestValheimPlusVersion();
+            ValheimPlusInstallStatus = "Download starting";
+            UpdateUI();
+
+            string ValheimPlusDownloadURL = $"https://github.com/valheimPlus/ValheimPlus/releases/download/{LatestValheimPlusVersion}/WindowsClient.zip";
+
+            //Download
+
+            if (File.Exists(ZipFullPath))
+            {
+                File.Delete(ZipFullPath);
+            }
+
+
+
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri(ValheimPlusDownloadURL), ZipFullPath);
+            }
+            catch
+            {
+                ValheimPlusInstallStatus = "Download Failed!";
+                UpdateUI();
+
+            }
+
+
+            while (!downloadComplete)
+            {
+                System.Windows.Forms.Application.DoEvents();
+            }
+
+            downloadComplete = false;
+
+            //Extraction
+            ValheimPlusInstallStatus = "Extracting...";
+            UpdateUI();
+
+            try
+            {
+                if (Directory.Exists(ZipExtractionPath))
+                {
+                   Directory.Delete(ZipExtractionPath);
+                }
+
+                ZipFile.ExtractToDirectory(ZipFullPath, ZipExtractionPath);
+
+            }
+            catch
+            {
+               ValheimPlusInstallStatus = "Extraction Failed!";
+                UpdateUI();
+                ExitandCleanup();
+                return;
+            }
+
+
+            //File copy
+            ValheimPlusInstallStatus = "Copying Files...";
+
+            try
+            {
+                //foreach (var file in Directory.GetFiles(ZipExtractionPath))
+                //    File.Copy(file, Path.Combine(ValheimGameFolder, Path.GetFileName(file)), true);
+
+
+                //Now Create all of the directories
+                foreach (string dirPath in Directory.GetDirectories(ZipExtractionPath, "*",
+                    SearchOption.AllDirectories))
+                    Directory.CreateDirectory(dirPath.Replace(ZipExtractionPath, ValheimGameFolder));
+
+                //Copy all the files & Replaces any files with the same name
+                foreach (string newPath in Directory.GetFiles(ZipExtractionPath, "*.*",
+                    SearchOption.AllDirectories))
+                    File.Copy(newPath, newPath.Replace(ZipExtractionPath, ValheimGameFolder), true);
+
+            }
+            catch
+            {
+                ValheimPlusInstallStatus = "Copy Failed!";
+                UpdateUI();
+                ExitandCleanup();
+                return;
+            }
+
+            ValheimPlusInstallStatus = "Installation Complete";
+
+            UpdateUI();
+            GetValheimPlusVersion();
+
+            ExitandCleanup();
+        }
+
+        private void UninstallValheimPlus()
+        {
+
+            try
+            {
+                File.Delete(ValheimPlusDll);
+                File.Delete($"{ValheimGameFolder}\\winhttp.dll");
+            }
+            catch
+            {
+                materialLabel5.Text = "Failed to remove Valhiem Plus!";
+                UpdateUI();
+            }
+
+
+            GetValheimPlusInstallStatus();
         }
 
 
@@ -145,23 +367,48 @@ namespace ZLheim_Modloader
         private void materialLabel3_Click(object sender, EventArgs e)
         {
 
+
         }
 
 
 
         private void Form1_Load_1(object sender, EventArgs e)
         {
-            ValheimGameFolder = LocateValhiemDirectory();
+            LocateValheimDirectory();
             materialLabel4.Text = ValheimGameFolder;
 
-            if (GetValhiemPlusInstallStatus(ValheimGameFolder))
-            {
-                materialLabel5.Text = "Valhiem Plus is Installed";
-            }
-            else
-            {
-                materialLabel5.Text = "Valhiem Plus is not Installed!";
-            }
+            GetValheimPlusInstallStatus();
+            GetValheimPlusVersion();
+            GetLatestValheimPlusVersion();
+
+        }
+
+        private void materialLabel6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialRaisedButton4_Click(object sender, EventArgs e)
+        {
+            UninstallValheimPlus();
+            UpdateUI();
+        }
+
+        private void materialLabel8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialLabel1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialRaisedButton1_Click(object sender, EventArgs e)
+        {
+
+            InstallValheimPlus();
+
         }
     }
 }
